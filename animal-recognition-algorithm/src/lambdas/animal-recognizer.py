b7ef63195ef9e5c1ce24b2cac3dd35c4b0f2e872
Ewaryst Lawecki <ewaryst.lawecki@tsh.io>
2024-05-06 23:34:55 +0200

    feat: connect lambda recognizer to sqs

--- animal-recognition-algorithm/src/lambdas/animal-recognizer.py
+++ animal-recognition-algorithm/src/lambdas/animal-recognizer.py
@@ -1,44 +1,75 @@
-import json
-
+import os
 import numpy as np
 import tensorflow as tf
+import boto3
+
+from datetime import datetime
 
 from config import CLASS_NAMES
 from get_img_from_base64 import get_img_from_base64 # type: ignore
 
+FRAME_DYNAMO_TABLE = os.environ["FRAME_DYNAMO_TABLE"]
+FRAME_S3_BUCKET = os.environ["FRAME_S3_BUCKET"]
+
+s3 = boto3.client('s3')
+dynamodb = boto3.client('dynamodb')
+
 def handler(event, _):
-    print(event)
+    print("Received event", event)
 
-    if 'image' not in event:
-        return {
-            'statusCode': 400,
-            'body': json.dumps('Missing image')
-        }
+    records = event["Records"]
+
+    for record in records:
     
-    image_base64 = event["image"]
+        frame_id = record["messageAttributes"]["FrameId"]["stringValue"]
 
-    model = tf.keras.models.load_model('models/model.keras')
+        image_base64 = get_image_from_s3_by_id(frame_id)
 
-    img = get_img_from_base64(image_base64)
-    
-    img_array = tf.keras.utils.img_to_array(img)
-    img_array = tf.expand_dims(img_array, 0) # Create a batch
+        model = tf.keras.models.load_model('models/model.keras')
 
-    predictions = model.predict(img_array)
-    score = tf.nn.softmax(predictions[0])
+        img = get_img_from_base64(image_base64)
+        
+        img_array = tf.keras.utils.img_to_array(img)
+        img_array = tf.expand_dims(img_array, 0) # Create a batch
 
-    predicted_class = CLASS_NAMES[np.argmax(score)]
-    prediction_confidence = 100 * np.max(score)
+        predictions = model.predict(img_array)
+        score = tf.nn.softmax(predictions[0])
 
-    print(
-        "This image most likely belongs to {} with a {:.2f} percent confidence."
-        .format(predicted_class, prediction_confidence)
-        )
-    
-    return {
-        'statusCode': 200,
-        'body': {
-            'predicted_class': predicted_class,
-            'prediction_confidence': prediction_confidence
-        }
+        predicted_class = CLASS_NAMES[np.argmax(score)]
+        prediction_confidence = 100 * np.max(score)
+
+        print(
+            "This image most likely belongs to {} with a {:.2f} percent confidence."
+            .format(predicted_class, prediction_confidence)
+            )
+
+        save_prediction_to_dynamo(frame_id, predicted_class, str(prediction_confidence))
+
+def get_image_from_s3_by_id(frame_id):
+    response = s3.get_object(Bucket=FRAME_S3_BUCKET, Key=f'{frame_id}.txt')
+    file_body = response['Body'].read().decode('utf-8')
+    return file_body
+
+def save_prediction_to_dynamo(frame_id, predicted_class, prediction_confidence):
+    primary_key = {'id': {'S': frame_id}}
+
+    current_datetime = datetime.now()
+    iso_string = current_datetime.isoformat()
+
+    update_values = {
+        ':updatedAt': {'S': iso_string},
+        ':processedAt': {'S': iso_string},
+        ':predictedClass': {'S': predicted_class},
+        ':predictionConfidence': {'N': prediction_confidence}
     }
+
+    update_expression = 'SET processedAt = :processedAt, predictedClass = :predictedClass, predictionConfidence = :predictionConfidence, updatedAt = :updatedAt'
+
+    dynamodb.update_item(
+        TableName=FRAME_DYNAMO_TABLE,
+        Key=primary_key,
+        UpdateExpression=update_expression,
+        ExpressionAttributeValues=update_values,
+        ConditionExpression='attribute_exists(id)',
+        ReturnValues='UPDATED_NEW'
+    )
