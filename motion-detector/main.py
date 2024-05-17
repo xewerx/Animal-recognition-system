479fda5cd5d6f1093ae76d0a8e3df2c359dfcdef
Piotr <piotr.polus@hotmail.com>
2024-05-17 14:43:24 +0200

    clear comments

--- motion-detector/main.py
+++ motion-detector/main.py
@@ -12,93 +12,58 @@ import base64
 from dotenv import load_dotenv
 import os
 
-video_path = os.path.join('.', 'input', 'tiger2.mp4')
-source = 'input/input.mp4' # nie działa ta wersja
-
-# Tworzenie obiektu kamery wideo, może być w nawiasie np (0,1,2,3 i przełączanie między nimi jeśli kamer/źródeł jest więcej)
-#Jeśli źródłem jest film lokalny, podajemy bezpośrednio link do pliku
-# cap = cv2.VideoCapture(video_path, cv2.CAP_DSHOW)
-# cap = cv2.VideoCapture(video_path, cv2.CAP_IMAGES)
+video_path = os.path.join('.', 'input', 'moose.mp4')
 
 cap = cv2.VideoCapture(video_path)
 
-#ustawienie obrazu
 cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
 cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
 
-# pobranie pierwszej klatki jako referencyjnej
 ret, start_frame = cap.read()
 
-
-# start_frame = imutils.resize(start_frame, width=500)
-# można zmieniać wymiary badanej klatki względem wielkości px otrzymanego wideo
-
 start_frame = cv2.cvtColor(start_frame, cv2.COLOR_BGR2GRAY)
-# konwertuje kolorową klatkę na klatkę szarą - do badania ruchu
-
 start_frame = cv2.GaussianBlur(start_frame, (21,21),0)
 alarm = False
-# zmienna do odpalania alarmu na przycisk
-alarm_mode = False
-
+alarm_mode = False  # variable to trigger an alarm on the button
 alarm_counter = 0
-# zmienna do ustalenia jak długo od pierwszego wykrycia ruchu ma miniąć, by alarm się załączył
-
 
 def detection_events():
-    # wszystko co chcemy robić po zauważeniu ruchu, w tej funkcji.
-    global alarm
+    # everything that happens when movement is noticed, in this function.    global alarm
     for _ in range(5):
         if not alarm_mode:
             break
         print("Movement noticed!")
-
         _, frame_to_send = cap.read()
 
-        # Zapisz obraz jako plik JPEG zakodowany w Base64
         if frame_to_send is not None:
             frame_to_send = save_snapshot_as_base64(frame_to_send, directory='./output_photos')
-        
-            # print(frame_to_send) #test: printuje jako tekst base64
+            load_dotenv()
+            api_key = os.getenv('API_KEY')
 
-            # load_dotenv()
-            # # Pobierz klucz API z pliku .env
-            # api_key = os.getenv('API_KEY')
-            
-            # # Przygotuj ciało żądania
-            
-            # body = {
-            #     "imageBase64": frame_to_send
-            # }
+            body = {
+                "imageBase64": frame_to_send
+            }
 
-            # # Nagłówki żądania
-            # headers = {
-            #     'x-api-key': api_key,
-            #     'Content-Type': 'application/json'
-            # }
+            headers = {
+                'x-api-key': api_key,
+                'Content-Type': 'application/json'
+            }
 
-            # # URL endpointu, na który wysyłasz żądanie (zmień na odpowiedni)
-            # # Pobierz URL z pliku .env
-            # url = os.getenv('ENDPOINT_URL')
-            # # url = 'https://api.yourservice.com/endpoint'
+            url = os.getenv('ENDPOINT_URL')
+            # url = 'https://api.yourservice.com/endpoint'
 
-            # # Wyślij żądanie POST
-            # response = requests.post(url, json=body, headers=headers)
+            response = requests.post(url, json=body, headers=headers)
 
-            # # Sprawdź odpowiedź
-            # if response.status_code == 200:
-            #     print('Success:', response.json())
-            # else:
-            #     print('Error:', response.status_code, response.text)
-            
-            # Pauza przez 10 sekund - do ustawienia przerwa nim rozpocznie wykrywanie kolejnego zwierzęcia
+            if response.status_code == 200:
+                print('Success:', response.json())
+            else:
+                print('Error:', response.status_code, response.text)
+
             time.sleep(10)
-
     alarm = False
 
 
 # MAIN LOOP
-
 while True:
     try:
         ret, frame = cap.read()
@@ -107,8 +72,6 @@ while True:
             print("Koniec filmu lub błąd odczytu.")
             break
         
-        # frame = imutils.resize(frame, width=500)
-
         if alarm_mode:
             frame_bw = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
             frame_bw = cv2.GaussianBlur(frame_bw, (5,5), 0)
@@ -116,7 +79,8 @@ while True:
             threshold = cv2.threshold(difference, 25, 255, cv2.THRESH_BINARY)[1]
             start_frame = frame_bw
         
-            if threshold.sum() > 500: #wartość ustala "czułośc" wykrywania ruchu - im mniej tym bardziej czuły
+            if threshold.sum() > 500: 
+            #value sets the "sensitivity" of motion detection - the lower it is, the more sensitive it is
                 alarm_counter += 1
             else:
                 if alarm_counter > 0:
@@ -125,7 +89,6 @@ while True:
             cv2.imshow('Video Stream', threshold)
         else:
             cv2.imshow('Video Stream', frame)
-            # Wyświetl strumień wideo w oknie
 
         if alarm_counter > 20:
             if not alarm:
@@ -145,7 +108,5 @@ while True:
         print(f"Wystąpił błąd: {e}")
         break
 
-
-# Zwolnij obiekt kamery wideo i zamknij okna
 cap.release()
 cv2.destroyAllWindows()
--- motion-detector/save_snapshot.py
+++ motion-detector/save_snapshot.py
@@ -5,48 +5,29 @@ import os
 
 directory='./output_photos'
 
-
 def save_snapshot_as_base64(image, directory):
     """
-    Zapisuje obraz z OpenCV jako plik JPEG zakodowany w Base64.
+    Saves an image from OpenCV as a Base64 encoded JPEG file.
 
     Args:
-        image: Obraz do zapisania.
-        directory (str): Ścieżka do katalogu, gdzie mają być zapisane obrazy.
+        image: Image to save.
+        directory (str): Path to the directory where the images are to be saved.
     Returns:
-        str: Zakodowany obraz w formacie Base64.
+        str: Encoded image in Base64 format.
     """
     if image is None:
         raise ValueError("Obraz jest pusty.")
     
     if not os.path.exists(directory):
         os.makedirs(directory)
-    
-    # Pobierz aktualną datę i godzinę
+
     current_time = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
-    
-    # Utwórz nazwę pliku na podstawie daty i godziny
+
     filename = f"{current_time}.jpg"
 
-    # # Zapisz obraz jako plik JPEG
     cv2.imwrite(cv2.os.path.join(directory, filename), image)
 
-    # # Wczytaj zapisany obraz do kodowania Base64
     with open(cv2.os.path.join(directory, filename), "rb") as image_file:
         encoded_image = base64.b64encode(image_file.read()).decode('utf-8')
 
     return encoded_image
-
-'''
-# Przykładowe użycie
-# Wczytaj obraz
-
-image = cv2.imread("example_image.jpg")
-
-# Zapisz obraz jako plik JPEG zakodowany w Base64
-encoded_image = save_snapshot_as_base64(image, directory='./')
-
-print("Obraz zakodowany w Base64:")
-print(encoded_image)
-
-'''
